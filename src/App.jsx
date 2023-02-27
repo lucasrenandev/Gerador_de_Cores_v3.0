d621a746f6fb28c952a4bcfab034ba7c23163db1
lucasrenandns <lucasrenandns96@gmail.com>
2023-02-26 21:27:40 -0300

    Gerador de Cores v3.0

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [box] = useState("box")
+  const [text] = useState("text")
+
+  function generateText() {
+    const textElement = document.querySelectorAll(".box .text")
+
+    textElement.forEach((span) => {
+      span.textContent = generateHexaColor()
+    })
+  }
+
+  function generateColors() {
+    const boxElement = document.querySelectorAll(".box")
+
+    boxElement.forEach((box) => {
+      box.style.backgroundColor = generateHexaColor()
+      generateText()
+    })
+  }
+
+  function generateHexaColor() {
+    const charactes = "123456789abcdef"
+    const maxCharacters = 6
+    let color = ""
+
+    for(let i = 0; i < maxCharacters; i ++) {
+      const randomColor = Math.floor(Math.random() * charactes.length)
+      color += charactes.substring(randomColor, randomColor + 1)
+    }
+    return "#" + color
+  }
+  
+  return (
+    <div className='container'>
+      <div className={box}><span className={text}></span></div>
+      <div className={box}><span className={text}></span></div>
+      <div className={box}><span className={text}></span></div>
+      <div className={box}><span className={text}></span></div>
+      <div className={box}><span className={text}></span></div>
+      <div className={box}><span className={text}></span></div>
+      <button type='button' onClick={generateColors}>Gerar</button>
+    </div>//End container
+  )
+}
+
+export default App
